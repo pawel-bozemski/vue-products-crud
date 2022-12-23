4379173329ad3d235bcf0584f6ff182123fa256e
pawel-bozemski <paw.boz@gmail.com>
2022-12-23 11:23:11 +0100

    Init project and store

---
+++ src/components/ProductList.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="hello">
+    <table class="table">
+      <thead class="thead-light">
+        <tr>
+          <th scope="col">Ref</th>
+          <th scope="col">Name</th>
+          <th scope="col">City</th>
+          <th scope="col">Price $</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <th scope="row">1</th>
+          <td>Mark</td>
+          <td>Otto</td>
+          <td>@mdo</td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "ProductList",
+  props: {
+    msg: String,
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
--- src/store/index.js
+++ src/store/index.js
@@ -1,9 +1,71 @@
 import { createStore } from "vuex";
 
 export default createStore({
-  state: {},
+  state: {
+    products: [
+      {
+        ref: 1,
+        name: "Product 1",
+        city: "Warsaw",
+        price: 12,
+      },
+      {
+        ref: 2,
+        name: "Product 2",
+        city: "Berlin",
+        price: 321,
+      },
+      {
+        ref: 3,
+        name: "Product 3",
+        city: "Lublin",
+        price: 157,
+      },
+      {
+        ref: 4,
+        name: "Product 4",
+        city: "Wrocław",
+        price: 125,
+      },
+      {
+        ref: 5,
+        name: "Product 5",
+        city: "Poznań",
+        price: 1,
+      },
+    ],
+  },
   getters: {},
-  mutations: {},
-  actions: {},
+  mutations: {
+    ADD_PRODUCT(state, payload) {
+      state.products.push({
+        ref: payload.ref,
+        name: payload.name,
+        city: payload.city,
+        price: payload.price,
+      });
+    },
+    EDIT_PRODUCT(state, payload) {
+      const product = state.products.find((item) => item.ref === payload.ref);
+      product.name = payload.name;
+      product.city = payload.city;
+      product.price = payload.price;
+    },
+    REMOVE_PRODUCT(state, payload) {
+      const i = state.products.map((item) => item.ref).indexOf(payload.ref);
+      state.products.splice(i, 1);
+    },
+  },
+  actions: {
+    addProduct({ commit }, payload) {
+      commit(`ADD_PRODUCT`, payload);
+    },
+    editProduct({ commit }, payload) {
+      commit(`EDIT_PRODUCT`, payload);
+    },
+    removeProduct({ commit }, payload) {
+      commit(`REMOVE_PRODUCT`, payload);
+    },
+  },
   modules: {},
 });
